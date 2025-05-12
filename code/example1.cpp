8270dbbf604f1a825044af737aef9105093c3ce2
Camilo René Duque Becerra <42359956+ProfCDuqueTec@users.noreply.github.com>
2025-05-11 18:23:07 -0600

    Update example1.cpp

--- code/example1.cpp
+++ code/example1.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 class Punto {
 public:
     int x, y;
@@ -11,5 +13,8 @@ public:
 
 int main() {
     Punto p1(1, 2), p2(3, 4);
+    cout << "Punto 1: (" << p1.x << ", " << p1.y << ")" << std::endl;
+    cout << "Punto 2: (" << p2.x << ", " << p2.y << ")" << std::endl;
     Punto p3 = p1 + p2;  // Uso del operador sobrecargado
+    cout << "Suma de puntos: (" << p3.x << ", " << p3.y << ")" << std::endl;
 }
